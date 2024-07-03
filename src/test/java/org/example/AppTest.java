e242f406334bba3eeca0b8642aa2ed6507f363e3
p.sattarzadeh <p.sattarzadeh@gmail.com>
2024-07-03 10:28:41 +0330

    1403-04-13:Creating the project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest
+public class AppTest
+{
+    @Test
+    public void contextLoad(){
+
+
+    }
+}
